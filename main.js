758acd6ad85b565d25e189fba9f81159cbb2729e
Bexultan_Tokan <Bexultan_Tokan>
2024-06-05 01:58:31 +0500

    major update

--- main.js
+++ main.js
@@ -4,46 +4,42 @@ leftLineY = document.getElementById('leftLine').getBoundingClientRect().top;
 rightLineX = document.getElementById('rightLine').getBoundingClientRect().left;
 rightLineY = document.getElementById('rightLine').getBoundingClientRect().top;
 var passed = new Array();
-// toBePassed = new Set();
 leftLine = document.getElementById('leftLine');
 rightLine = document.getElementById('rightLine');
 
 
-    
-startButton = null;
+timeOutInterval = 4000;
+idCounter = 0
+duration = 2
 offset = 20 // between the lines
 incline = 10 // Y offset for the right line
+
+let leftTimerResolve;
+let rightTimerResolve;
+let breakLeftLoop;
 blocks = 0
+startButton = null;
+var receivedRA = false
+var connectionEnded = false
+var rightSatteliteTriggered = false
+var RSreceived = false
 var leftPackets = [], rightPackets = [];
-
+var leftTimer, rightTimer;
+var arrOfUndelivered = Array();
 var numbersToDeliver = Array()
+dictionaryOfLines = {}
 
+function makeLineStruct(dataNum, isCP, type, requiredNums) {
+    this['dataNum'] = dataNum;
+    this['isCP'] = isCP;
+    this['type'] = type;
+    this['requiredNums'] = requiredNums;
+}
 function showMessage(message) {
     document.getElementById('message').textContent = message;
 }
 
-// function handleFormSubmit(event) {
-//     event.preventDefault(); // Prevent the default form submission
-
-//     var redBlocks = document.getElementById('redBlocks').value; // Get the number value from the input field
-//     var greenBlocks = document.getElementById('greenBlocks').value; 
-//     showMessage("There are " + redBlocks + " red blocks and " + greenBlocks + " green blocks.");
-
-//     var numOfBlocks = parseInt(redBlocks) + parseInt(greenBlocks);
-//     leftLine.style.height = Math.max(numOfBlocks * (offset + 50) , 60) + 'px';
-//     rightLine.style.height = Math.max(numOfBlocks * (offset + 50) , 60) + 'px';
-
-//     document.querySelectorAll('.line').forEach(e => e.remove());
-//     leftLineY = leftLine.getBoundingClientRect().top;
-//     rightLineY = rightLine.getBoundingClientRect().top;
-//     // You can add more JavaScript code here to use the number as needed
-//     createBlocks(redBlocks, greenBlocks);  
-//     enableButton();
-//     document.getElementById('container1').style.visibility = "visible";
-
-// }
-
-async function handleFormSubmit2(event) {
+async function handleFormSubmit(event) {
     event.preventDefault(); // Prevent the default form submission
     blocks = parseInt(document.getElementById('2blocks').value); 
     showMessage("There are " + blocks + " blocks to transmit.");
@@ -56,143 +52,179 @@ async function handleFormSubmit2(event) {
     rightLineY = rightLine.getBoundingClientRect().top;
     numbersToDeliver = Array.from({ length: blocks }, (_, i) => i + 1);
     packetsToBeDelivered = blocks;
-    // for (var i = 0; i < blocks; i++) {
-    //     toBePassed.add(i);
-    // }
+    await leftSattelite();
     // do {
     //     console.log("Packets to be delivered: ", packetsToBeDelivered);
-    //     total = new Set(toBePassed) 
+    //     console.log(numbersToDeliver);
+    //     total = packetsToBeDelivered
     //     for (var i = 0; i < total; i++) {
-    //         await moveLine('blue', leftLineX, leftLineY, rightLineX, rightLineY + incline, 1, () => {});
+    //         labelNum = numbersToDeliver.shift();
+    //         console.log("num: "+labelNum);
+    //         if (i == total - 1){
+    //             dictionaryOfLines[idCounter] = new makeLineStruct(labelNum, true, 'data', []);
+    //         } else {
+    //             dictionaryOfLines[idCounter] = new makeLineStruct(labelNum, false, 'data', []);
+    //         }
+    //         await moveLine('blue', leftLineX, leftLineY, rightLineX, rightLineY + incline, 1, labelNum, () => {});
     //         packetsToBeDelivered -= 1;
     //     }
-    //     await moveLine('yellow', rightLineX, rightLineY, leftLineX , leftLineY + 15, 1, () => {});
+    //     dictionaryOfLines[idCounter] = new makeLineStruct(-1, false, 'RS', [].concat(numbersToDeliver));
+    //     await moveLine('yellow', rightLineX, rightLineY, leftLineX , leftLineY + 15, 1, -1, () => {});
     // } 
-    // while (toBePassed.length != 0);
-
-    do {
-        console.log("Packets to be delivered: ", packetsToBeDelivered);
-        console.log(numbersToDeliver);
-        total = packetsToBeDelivered
-        for (var i = 0; i < total; i++) {
-            labelNum = numbersToDeliver.shift();
-            console.log("num: "+labelNum);
-            await moveLine('blue', leftLineX, leftLineY, rightLineX, rightLineY + incline, 1, labelNum, () => {});
-            packetsToBeDelivered -= 1;
-        }
-        await moveLine('yellow', rightLineX, rightLineY, leftLineX , leftLineY + 15, 1, -1, () => {});
-    } 
-    while (packetsToBeDelivered != 0);
-    await moveLine('red', leftLineX, leftLineY, rightLineX, rightLineY + 15, 1, -1, () => {});
+    // while (packetsToBeDelivered != 0);
+    // dictionaryOfLines[idCounter] = new makeLineStruct(-1, false, 'RA', []);
+    // await moveLine('red', leftLineX, leftLineY, rightLineX, rightLineY + 15, 1, -1, () => {});
 }
 
+async function sendBatch() {
+    arrOfUndelivered = Array();
+    total = numbersToDeliver.length
 
-// const blocksContainer = document.getElementById('blocksContainer');
+    for (var i = 0; i < total; i++) {
+        labelNum = numbersToDeliver[i];
+        console.log("num: "+labelNum);                                                               
+        if (i == total - 1){
+            dictionaryOfLines[idCounter] = new makeLineStruct(labelNum, true, 'data', []);
+            moveLine('blue', leftLineX, leftLineY, rightLineX, rightLineY + incline, duration, labelNum, callbackCP);
+        } else {
+            dictionaryOfLines[idCounter] = new makeLineStruct(labelNum, false, 'data', []);
+            moveLine('blue', leftLineX, leftLineY, rightLineX, rightLineY + incline, duration, labelNum, () => {});
+        }
+        await waitForDuration();
+        packetsToBeDelivered -= 1;
+    }  
+}
 
-
-
-// function createBlocks(redNum, greenNum) {
-//     blocksContainer.style.position = 'relative'; // Set the container to relative position
-//     blocksContainer.innerHTML = ''; // Clear the container first
-    
-//     for (var i = 0; i < redNum; i++) 
-//         createSingleBlock('red');
-
-//     for (var i = 0; i < greenNum; i++) 
-//         createSingleBlock('green');
-// }
-
-// function createSingleBlock(color)
-// {
-//     var block = document.createElement('div');
-//     block.className = 'block';
-//     block.style.backgroundColor = color;
-//     blocksContainer.appendChild(block);
-// }   
-
-
-// function movingLastBlock(button) {
-//     startButton = button;
-//     startButton.disabled = true;
-
-//     const lastBlock = blocksContainer.lastElementChild;
-//     const endPosition = blocksContainer.offsetWidth - lastBlock.getBoundingClientRect().left; // End position
-//     lastBlock.style.transform = `translateX(${endPosition}px)`; // Apply the transformation
-//     lastBlock.addEventListener('transitionend', () => {
-//         lastBlock.remove(); // Remove the last block
-//         const color = lastBlock.style.backgroundColor;
-        
-//         // movingLine(color, color == 'red');
-
-
-//         if(color == 'red')
-//         {
-//             const backLineColor = 'rgb(255, 217, 0)'
-//             for (var i = 0; i < 8; i++) {
-//                 if (i == 7)
-//                     moveLine('red', leftLineX, leftLineY + i * 4, rightLineX, rightLineY + 10 + i * 4, 1, () => {
-//                         moveLine(backLineColor, rightLineX, rightLineY + i * 4 + 10, leftLineX, leftLineY + i * 4 + 40, 1, () => {
-//                             enableButton();
-//                             leftLineY += i * 8 + 40;
-//                             rightLineY += i * 8 + 40;
-//                         });
-//                     });
-//                 else
-//                     moveLine('red', leftLineX, leftLineY + i * 4, rightLineX, rightLineY + 10 + i * 4, 1, () => {});
-//             }
-//             // moveLine('red', leftLineX, leftLineY, rightLineX, rightLineY + 10, 0.5, () => {
-//             //     moveLine(backLineColor, rightLineX, rightLineY + 10, leftLineX, leftLineY + 40, 0.5, () => {
-//             //         enableButton();
-//             //         leftLineY += offset;
-//             //         rightLineY += offset;
-//             //     });
-//             // });
-//         }
-//         else
-//         {
-//             for (var i = 0; i < 8; i++) {
-//                 if (i == 7)
-//                     moveLine('green', leftLineX, leftLineY + i * 4, rightLineX, rightLineY + 10 + i * 4, 1, () => {
-//                             enableButton();
-//                             leftLineY += offset + 4 * 8;
-//                             rightLineY += offset + 4 * 8;
-//                     });
-//                 else
-//                     moveLine('green', leftLineX, leftLineY + i * 4, rightLineX, rightLineY + 10 + i * 4, 1, () => {});
-//             }
-
-//             // moveLine('green',  leftLineX, rightLineY,rightLineX, rightLineY + 10, 0.5, () => {
-//             //     enableButton();
-//             //     leftLineY += offset;
-//             //     rightLineY += offset;
-//             // });
-//         }
-//         document.querySelector('.line').addEventListener('click', function() {
-//             alert('Object clicked during transition!');
-//         });
-//     });
-// }
-
-
-// function moveLine(color, fromX, fromY, toX, toY, duration, callback) {
-//     const rightDiv = document.getElementById('rightSide');
-//     var line = document.createElement('div');
-//     line.className = 'line';
-//     rightDiv.appendChild(line);
-
-//     var arrow = document.createElement('div');
-//     // arrow.className = 'triangle';
-//     // line.appendChild(arrow);
+async function leftSattelite() {
+    leftLineY = leftLine.getBoundingClientRect().top;
+    rightLineY = rightLine.getBoundingClientRect().top;
+    blocks = parseInt(document.getElementById('2blocks').value); 
+    numbersToDeliver = Array.from({ length: blocks }, (_, i) => i + 1);
+    packetsToBeDelivered = blocks;
 
     
-//     line.style.left = fromX + 'px';
-//     line.style.top = fromY + 'px';
-//     line.style.backgroundColor = color;
+    do {
+        // make a while loop which sends last packet with CP while RA was not sent
+        console.log('before sending batch', numbersToDeliver)
+        await sendBatch();
+        RSreceived = false;
+        await waitForTimeout();
+        console.log('RSreceived', RSreceived)
+        while (RSreceived == false && numbersToDeliver.length > 0) {
+            moveLine('blue', leftLineX, leftLineY, rightLineX, rightLineY + incline, duration, numbersToDeliver[numbersToDeliver.length - 1], callbackCP);
+            await waitForDuration()
+            await waitForTimeout();
+            console.log('inside loop', numbersToDeliver)
+        }
+        console.log('numbers to send', numbersToDeliver)
+    }
+    while (numbersToDeliver.length > 0) // every time RA was sent
+    // while (connectionEnded == false) {
+    //     console.log('try to end connection')
+    //     moveLine('yellow', leftLineX, leftLineY, rightLineX, rightLineY + incline, duration, -1, () => {RSreceived = true});
+    //     await waitForDuration();
+    //     await waitForTimeout();
+    // }
+    console.log('leftSattelite finished')
+}
 
-//     extendLineTo(toX, toY, line, arrow, duration)
-//     line.addEventListener('transitionend', callback);
-
+// function endConnection() {
+//     connectionEnded = true;
 // }
+
+async function sendRS() {
+    dictionaryOfLines[idCounter] = new makeLineStruct(-1, false, 'RS', arrOfUndelivered);
+    moveLine('red', rightLineX, rightLineY, leftLineX, leftLineY + incline, duration, -1, callbackRA);
+    await waitForDuration();
+}
+
+async function sendRA() {
+    dictionaryOfLines[idCounter] = new makeLineStruct(-1, false, 'RA', []);
+    moveLine('yellow', leftLineX, leftLineY, rightLineX, rightLineY + incline, duration, -1, () => {receivedRA = true});
+    await waitForDuration();
+}
+
+async function rightSattelite() { // TODO
+    while (arrOfUndelivered.length > 0) {
+        console.log('arrOfUndelivered', arrOfUndelivered)
+        await sendRS();
+        await waitForRightTimeout()
+    }
+    // while RA isn't received
+    while (receivedRA == false) {
+        await sendRS();
+        await waitForRightTimeout()
+    }        
+
+    console.log('rightSattelite finished', receivedRA)
+    // leftLineY = leftLine.getBoundingClientRect().top;
+    // rightLineY = rightLine.getBoundingClientRect().top;
+    // packetsToBeDelivered = blocks;
+}
+
+function waitForTimeout() {
+    return new Promise(resolve => {
+        leftTimerResolve = resolve;
+        leftTimer = setTimeout(() => {
+            resolve();
+        }, timeOutInterval);
+    });
+}
+
+function waitForRightTimeout() {
+    return new Promise(resolve => {
+        rightTimerResolve = resolve;
+        rightTimer = setTimeout(() => {
+            resolve();
+        }, timeOutInterval);
+    });
+}
+
+function waitForDuration() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, duration * 1000);
+    });
+}
+
+function callbackRA(line) { // TODO
+    console.log("RA callback, RSreceived", RSreceived);
+    RSreceived = true;
+    console.log("RA callback, new RSreceived", RSreceived);
+    id = parseInt(line.id);
+    console.log("RA callback, id", id);
+    // console.log(line.id, dictionaryOfLines)
+    // console.log("arr from RA", dictionaryOfLines[id]['requiredNums'])
+    console.log("arr from RA", dictionaryOfLines[id]['requiredNums'])
+    numbersToDeliver = dictionaryOfLines[id]['requiredNums']
+    console.log("numbersToDeliver", numbersToDeliver)
+    if (numbersToDeliver.length == 0){
+        sendRA();
+    }
+    else {
+    clearTimeout(leftTimer);
+        if (leftTimerResolve) {
+            leftTimerResolve();  // Resolve the promise if it exists
+            leftTimerResolve = null;  // Reset the resolve function
+        }
+    }
+}
+
+function callbackCP() { // TODO
+    console.log("CP callback");
+    if (rightSatteliteTriggered == false){
+        rightSatteliteTriggered = true;
+        rightSattelite();
+    }
+    else{
+        clearTimeout(rightTimer);
+        if (rightTimerResolve) {
+            rightTimerResolve();  // Resolve the promise if it exists
+            rightTimerResolve = null;  // Reset the resolve function
+        }
+    }
+}
 
 function moveLine(color, fromX, fromY, toX, toY, duration, labelNum, callback) {
     leftLineY += offset;
@@ -214,6 +246,9 @@ function moveLine(color, fromX, fromY, toX, toY, duration, labelNum, callback) {
 
         const rightDiv = document.getElementById('rightSide');
         var line = document.createElement('div');
+        line.id = idCounter;
+        // console.log(dictionaryOfLines[idCounter])
+        idCounter += 1
         line.className = 'line';
         rightDiv.appendChild(line);
         line.style.left = fromX + 'px';
@@ -223,14 +258,15 @@ function moveLine(color, fromX, fromY, toX, toY, duration, labelNum, callback) {
 
         // Function to stop line movement
         line.addEventListener('click', function() {
-            if (passed.includes(labelNum)){
+            if (passed.includes(line.id)){
                 return;
             }  
             this.style.width = `${this.offsetWidth}px`; // Set the width to its current value
             this.style.transition = 'none'; // Remove any transitions
             packetsToBeDelivered += 1;
             
-            numbersToDeliver.push(labelNum)
+            if (labelNum != -1)
+                arrOfUndelivered.push(labelNum)
 
             const src = 'mark.png';
             const x = this.style.left;
@@ -243,15 +279,18 @@ function moveLine(color, fromX, fromY, toX, toY, duration, labelNum, callback) {
             img.style.width = '18px';
             document.getElementById('rightSide').appendChild(img);
 
-            console.log('Line movement stopped at width:', this.style.width);
+            // console.log('Line movement stopped at width:', this.style.width);
             resolve();
         });
 
         extendLineTo(toX, toY, line, duration); // Changed the function signature for simplicity
 
         line.addEventListener('transitionend', () => {
-            callback();
-            passed.push(labelNum);
+            passed.push(line.id);
+            if (arrOfUndelivered.includes(labelNum)){
+                arrOfUndelivered.splice(arrOfUndelivered.indexOf(labelNum), 1); 
+            } 
+            callback(line);
             resolve(); // Resolve the promise when the transition ends
         });
     });
@@ -264,27 +303,7 @@ function enableButton()
     startButton.disabled = false;
 
 }   
-// function extendLineTo(x, y, line, arrow, duration) {
-//     const scrollY = window.scrollY;
-//     const scrollX = window.scrollX;
-//     const lineLeft = line.getBoundingClientRect().left + scrollX;
-//     const lineTop = line.getBoundingClientRect().top + scrollY;
 
-//     const deltaX = x - lineLeft;
-//     const deltaY = y - lineTop;
-//     const length = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-//     const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
-
-//     // Apply the animation to the line
-//     line.style.width = `${length}px`;
-//     line.style.transform = `rotate(${angle}deg)`;
-//     line.style.transition = `width ${duration}s linear`;
-    
-//     arrow.style.transform = `rotateX(${angle + 90}deg)`;
-//     arrow.style.transform = `translateX(${length}px)`;
-//     arrow.style.transition = `transform ${duration}s linear`;
-//     console.log(line)
-// }
 function extendLineTo(x, y, line, duration) {
     const scrollY = window.scrollY;
     const scrollX = window.scrollX;
